27dbf734b24270b23009bcb88318f037f22c8259
MadJoker0815 <57637291+MadJoker0815@users.noreply.github.com>
2020-01-10 16:47:05 +0100

    Create logclean.sh
    file found in valetudo prebuilt firmware RE-0.8.1

---
+++ opt/rockrobo/rrlog/logclean.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "`date` ---"
+
+if [ ! -d "$1" ]; then
+  echo '"'"$1"'": not a directory'
+  exit
+fi
+
+items="$1"/*
+for item in $items
+do
+  if [ -d "$item" ]; then
+    echo "Removing directory $item..."
+    rm -fr $item
+  elif [ -f "$item" ]; then
+    if [[ $item == *".log" ]]; then
+      echo "Shrinking log $item..."
+      echo "$(tail -20 "$item")" > $item
+    else
+      echo "Removing file $item..."
+      rm "$item"
+    fi
+  fi
+done
