b347d46a03e692e68a7a9d22d387661d9405d4b2
MadJoker0815 <57637291+MadJoker0815@users.noreply.github.com>
2020-01-10 17:18:02 +0100

    Update logclean.sh

--- opt/rockrobo/rrlog/logclean.sh
+++ opt/rockrobo/rrlog/logclean.sh
@@ -12,11 +12,11 @@ for item in $items
 do
   if [ -d "$item" ]; then
     echo "Removing directory $item..."
-    rm -fr $item
+    rm -fr "$item"
   elif [ -f "$item" ]; then
     if [[ $item == *".log" ]]; then
       echo "Shrinking log $item..."
-      echo "$(tail -20 "$item")" > $item
+      echo "$(tail -20 "$item")" > "$item"
     else
       echo "Removing file $item..."
       rm "$item"
